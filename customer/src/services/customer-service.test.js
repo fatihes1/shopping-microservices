3237e3a88226d5b356eeb7ee6541156fea741b5d
Es <fatih@itsmoneo.com>
2024-03-31 18:37:14 +0300

    check workflow work correctly

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -3,6 +3,7 @@ describe("CustomerService", () => {
     // which function it is
     describe("SignIn", () => {
         // which scenario we are testing
+        // Added this additional test to test CI Workflow working
         test("Validate user inputs", () => {
 
         })
