3a6fff7e99af079d53961d277e8a5aaea907641e
Es <fatih@itsmoneo.com>
2024-03-31 18:35:29 +0300

    added tests to package.json and created test file for customer-service

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,13 @@
+// which service it is
+describe("CustomerService", () => {
+    // which function it is
+    describe("SignIn", () => {
+        // which scenario we are testing
+        test("Validate user inputs", () => {
+
+        })
+        test("Validate response", async () => {
+
+        })
+    })
+})
