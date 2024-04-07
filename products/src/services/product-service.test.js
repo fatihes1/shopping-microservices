eee17b8515ed01cede6d4454a81e060a1e4adadf
Es <fatih@itsmoneo.com>
2024-04-08 02:39:53 +0300

    added DUMMY tests for workflow

---
+++ products/src/services/product-service.test.js
@@ -0,0 +1,24 @@
+
+describe("ProductService", () => {
+    describe("CreateProduct", () => {
+        test("Validate product inputs", () => {
+
+        })
+        test("Validate response", async () => {
+
+        })
+    })
+    describe("GetProducts", () => {
+        test("Validate response", async () => {
+
+        })
+    })
+    describe("GetProductDescription", () => {
+        test("Validate product ID", () => {
+
+        })
+        test("Validate response", async () => {
+
+        })
+    })
+})
